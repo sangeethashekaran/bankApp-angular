09d9f10acd9839798075f87c44fb228e8dc64cca
sangeetha <sangeethashekaran27@gmail.com>
2021-05-17 23:08:15 +0530

    angular routing and dependency injection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 // import { AnyARecord } from 'dns';
 
 @Component({
@@ -9,45 +12,49 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   accno = "Account number please";
-  pswd = " ";
-  accountDetails:any = {
+  pswd = "";
+  accountDetails: any = {
     1000: { acno: 1000, actype: "savings", username: "userone", password: "userone", balance: 50000 },
     1001: { acno: 1001, actype: "savings", username: "usertwo", password: "usertwo", balance: 5000 },
     1002: { acno: 1002, actype: "current", username: "userthree", password: "userthree", balance: 10000 },
     1003: { acno: 1003, actype: "current", username: "userfour", password: "userfour", balance: 6000 }
-}
+  }
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
   // accnoChange(event:any) {
-    // this.accno = event.target.value;
-    // console.log(this.accno);
-    // 
+  // this.accno = event.target.value;
+  // console.log(this.accno);
+  // 
   // }
-  
+
   // pwdChange(event:any){
-    // this.pwd = event.target.value;
-    // console.log(this.pwd);
-    
-  
+  // this.pwd = event.target.value;
+  // console.log(this.pwd);
+
+
   login() {
-        let accno = this.accno;
-        let paswd = this.pswd;
-        let users = this.accountDetails;
-        if (accno in users) {
-            if(paswd == users[accno]["password"]) {
-              alert("Login succesful")
-
-            }
-            else {
-              alert("incoorect password");
-            }
-          }
-        else {
-          alert("invalid account")
-        }
-
-}
+    let accno = this.accno;
+    let pswd = this.pswd;
+    let users = this.accountDetails;
+    if(accno in users){
+       if(pswd==users[accno]["password"]) {
+         alert("login success")
+         this.router.navigateByUrl("dashboard")
+       }
+       else {
+         alert("incorrect password")
+       }
+    }
+    else {
+      alert("invalid account")
+    }
+    
+    
+ }
+ register() {
+   this.router.navigateByUrl("register")
+ }
 }
